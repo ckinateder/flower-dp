1c008cd4e50eac9a5090eafac7e01246fa0e92ad
Calvin Kinateder <calvinkinateder1@yahoo.com>
2022-01-26 14:47:59 -0500

    add clipping for pytorch

---
+++ privacy.py
@@ -0,0 +1,30 @@
+import torch
+
+
+def clip_parameter(parameter: torch.Tensor, clip_threshold: float) -> torch.Tensor:
+    """[summary]
+
+    >>> c = torch.tensor([[1, 2, 3], [-1, 1, 4]], dtype=torch.float)
+    >>> clip_parameter(c, 5)
+    tensor([[ 0.8839,  1.7678,  2.6517],
+            [-0.8839,  0.8839,  3.5355]])
+
+    Args:
+        parameter (torch.Tensor): input parameter
+        clip_threshold (float): C value
+
+    Returns:
+        torch.Tensor: clipped parrameter
+    """
+    # using formula for page 4, algorithm 1, line 7 in https://arxiv.org/pdf/1911.00222.pdf
+    clipped_parameter = parameter / max(1, torch.norm(parameter) / clip_threshold)
+    return clipped_parameter
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
+    c = torch.tensor([[1, 2, 3], [-1, 1, 4]], dtype=torch.float)
+    cp = clip_parameter(c, 5)
+    print(cp)
